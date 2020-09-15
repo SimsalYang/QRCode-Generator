353f8b20817cdef137c2c0b69324d18c7aa40d6a
SimsalYang <nicolas.yoong@gmail.com>
2020-09-15 22:30:40 +0800

    completed generate qrcode

---
+++ QRCode-Generator/QRCode-WPF/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using QRCoder;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace QRCode_WPF {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void generate_Click(object sender, RoutedEventArgs e) {
+            string name = "Name: " + username.Text.Trim();
+            string g = "Gender: " + gender.Text.Trim();
+            string gString = name + "\r\n" + g;
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(gString, QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData);
+            Bitmap qrCodeImage = qrCode.GetGraphic(20);
+
+            qrcode.Source = BitmapToImageSource(qrCodeImage);
+        }
+
+        BitmapImage BitmapToImageSource(Bitmap bitmap) {
+            using (MemoryStream memory = new MemoryStream()) {
+                bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
+                memory.Position = 0;
+                BitmapImage bitmapimage = new BitmapImage();
+                bitmapimage.BeginInit();
+                bitmapimage.StreamSource = memory;
+                bitmapimage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapimage.EndInit();
+
+                return bitmapimage;
+            }
+        }
+    }
+}
